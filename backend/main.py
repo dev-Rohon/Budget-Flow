7dd5a43ef0e70b4ebeadc2b607cf80d629ff6cba
Rohan Sekh <ytrohon33@gmail.com>
2026-09-18 12:03:26 +0530

    Allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,11 @@ from models.transaction import Transaction
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://budget-flow-nine-iota.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
